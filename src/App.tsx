19bde7cc8aac9e56eb964553877d75eca30e35be
aamcodes <aaronmendezcodes@gmail.com>
2023-06-12 08:59:27 -0400

    Added style setup with scss modules for demonstration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './styles/_globals.module.scss';
 
 export default function App() {
 	return (
-		<div id="__App_Container">
+		<div id="__App_Container" className={styles.app_container}>
 			<h1>Hello, Clover!</h1>
 		</div>
 	);
