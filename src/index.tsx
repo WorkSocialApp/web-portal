fb6b828dff6c7ab421c533a7d86ac6b4c4cad3ae
aamcodes <aaronmendezcodes@gmail.com>
2023-06-14 00:23:23 -0400

    Added React Router v6

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
-root.render(<App />);
+root.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
