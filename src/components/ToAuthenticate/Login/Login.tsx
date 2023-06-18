70542b6c96a61bea515164a4e28840fda89fa28c
aamcodes <aaronmendezcodes@gmail.com>
2023-06-18 19:05:00 -0400

    Added temporary background so that i dont go crazy from brightness during dev and proper form styling

--- src/components/ToAuthenticate/Login/Login.tsx
+++ src/components/ToAuthenticate/Login/Login.tsx
@@ -4,29 +4,35 @@ import styles from './Login.module.scss';
 export const Login = ({ onChange, loginForm, submitLogin }) => {
 	// console.log(loginForm);
 	return (
-		<div className={styles}>
+		<div className={styles.login_container}>
 			<form onSubmit={submitLogin}>
-				<div>
-					<label htmlFor="login_email">Email</label>
-					<input
-						value={loginForm.email}
-						onChange={onChange}
-						id="login_email"
-						type="email"
-						name="email"
-					/>
+				<div className={styles.form_group}>
+					<div className={styles.form_control}>
+						<label htmlFor="login_email">Email</label>
+						<input
+							value={loginForm.email}
+							onChange={onChange}
+							id="login_email"
+							type="email"
+							name="email"
+							placeholder="Enter your email address"
+						/>
+					</div>
+					<div className={styles.form_control}>
+						<label htmlFor="login_password">Password</label>
+						<input
+							value={loginForm.password}
+							onChange={onChange}
+							id="login_password"
+							type="password"
+							name="password"
+							placeholder="Enter your password"
+						/>
+					</div>
+					<div className={styles.form_control}>
+						<button className={styles.submit}>Submit</button>
+					</div>
 				</div>
-				<div>
-					<label htmlFor="login_password">Password</label>
-					<input
-						value={loginForm.password}
-						onChange={onChange}
-						id="login_password"
-						type="password"
-						name="password"
-					/>
-				</div>
-				<button>Submit</button>
 			</form>
 		</div>
 	);
